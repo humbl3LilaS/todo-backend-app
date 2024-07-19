559816b2f1d8204f462a009d7d259f71e36cfa6e
humbl3LilaS <sabishinekobebe@gmail.com>
2024-07-19 20:52:03 +0630

    add: cors support

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,28 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import "dotenv/config";
 import mongoose from "mongoose";
-import {todoRouter} from "./routes/Todo";
-import {authRouter} from "./routes/Auth";
+import { todoRouter } from "./routes/Todo";
+import { authRouter } from "./routes/Auth";
+import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/todos", todoRouter);
 app.use("/api/v1/auth", authRouter);
 app.get("/", async (req, res) => {
-    res.send("hello");
+  res.send("hello");
 });
 
-
 try {
-    mongoose.connect(process.env.DB_URI as string).then(r => console.log("DB server connected"));
-    app.listen(PORT, () => {
-        console.log(`Server is listening at port ${PORT}`);
-    });
-} catch (e) {
-}
+  mongoose
+    .connect(process.env.DB_URI as string)
+    .then((r) => console.log("DB server connected"));
+  app.listen(PORT, () => {
+    console.log(`Server is listening at port ${PORT}`);
+  });
+} catch (e) {}
